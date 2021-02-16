cc4eb6ad47ff30e40b062004988fddac45acee7a
Your Name <you@example.com>
2021-02-16 19:07:20 +0500

    teleport NW

---
+++ Assets/Scripts/TeleportControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TeleportControl : MonoBehaviour
+{
+    public VRTeleporter teleporter;
+    public SteamVR_Action_Boolean trackPad = SteamVR_Actions.default_Teleport;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (trackPad.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+        if (trackPad.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            teleporter.ToggleDisplay(true);
+        }
+    }
+}
